ac2099045bc610ff5d7cb6903a824d1ebec45ae8
JandroS12 <79588216+JandroS12@users.noreply.github.com>
2021-02-26 19:05:58 +0100

    Laser Project
    Laser Project (Color Change and Angular activation)

---
+++ Laser Game/Assets/Scripts/Angular.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angular : MonoBehaviour
+{
+    public GameObject angular;
+    public GameObject receptor;
+    public GameObject FirePointAngular;
+    public LineRenderer lr;
+
+    public bool Encendido = false;
+
+    void Start()
+    {
+        receptor = null;
+    }
+
+    void Update()
+    {
+
+        if (Encendido == true)
+        {
+            
+            FirePointAngular.SetActive(true);
+            Fire();
+
+        }
+        else
+        {
+            FirePointAngular.SetActive(false);
+
+            if (angular != null)
+            {
+                angular.GetComponent<Angular>().Encendido = false;
+            }
+            else if (receptor != null)
+            {
+                receptor.GetComponent<Receptor>().Encendido = false;
+            }
+        }
+    }
+
+    void Fire()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(FirePointAngular.transform.position, FirePointAngular.transform.forward, out hit))
+        {
+            if (hit.collider.gameObject.tag == "Enemy")
+            {
+
+                angular = hit.collider.gameObject;
+
+                hit.collider.gameObject.GetComponent<Angular>().Encendido = true;
+                receptor = null;
+                angular.GetComponent<Angular>().LastLaser(FirePointAngular.GetComponent<LineRenderer>());
+
+                FirePointAngular.GetComponent<LineRenderer>().material = lr.material;
+            }
+            else if (hit.collider.gameObject.tag == "Receptor")
+            {
+
+                receptor = hit.collider.gameObject;
+
+                hit.collider.gameObject.GetComponent<Receptor>().Encendido = true;
+                angular = null;
+                receptor.GetComponent<Receptor>().LastLaser(FirePointAngular.GetComponent<LineRenderer>());
+
+                FirePointAngular.GetComponent<LineRenderer>().material = lr.material;
+
+            }
+
+        }
+        else
+        {
+            FirePointAngular.GetComponent<LineRenderer>().material = lr.material;
+
+            if (angular != null)
+            {
+                angular.GetComponent<Angular>().Encendido = false;
+            }
+            else if (receptor != null)
+            {
+                receptor.GetComponent<Receptor>().Encendido = false;
+            }
+        }
+
+
+        
+
+    }
+    public void LastLaser(LineRenderer laser)
+    {
+        lr = laser;
+    }
+}
---
+++ Laser Game/Assets/Scripts/Laser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public LineRenderer lr;
+
+
+    private void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.useWorldSpace = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+        
+        if( Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z + hit.distance + 0.5f));
+                lr.SetPosition(0, new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z));
+                
+            }
+            
+        }
+        else
+        {
+            lr.SetPosition(0, new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z));
+            lr.SetPosition(1, new Vector3(transform.localPosition.x, transform.localPosition.y,transform.localPosition.z + 5000));
+
+            
+        }
+
+       
+    }
+}
