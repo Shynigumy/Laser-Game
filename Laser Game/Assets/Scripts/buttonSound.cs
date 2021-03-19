9f2265d5df0f8ecb454c543ffd3a8b0f2ab1da5c
Shynigumy <79588212+Shynigumy@users.noreply.github.com>
2021-03-19 17:55:39 +0100

    Audio Beta
    First sounds added

---
+++ Laser Game/Assets/Scripts/buttonSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonSound : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public AudioClip btnSound;
+
+    public void clickSound()
+    {
+        AudioSource.PlayOneShot(btnSound);
+    }
+}
