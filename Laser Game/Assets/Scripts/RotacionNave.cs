798d31cedcbbcc86b992c47d80535af5b50bfca0
JeanPierreSantMarc <j.pierre@santmarc.com>
2021-03-21 16:46:14 +0100

    ScriptRotacion

--- Laser Game/Assets/Scripts/RotacionNave.cs
+++ Laser Game/Assets/Scripts/RotacionNave.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class RotacionNave : MonoBehaviour
 {
+
+    public float X = 15f;
+    public float Y = -15f;
+    public float Z = -10f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15f, -15f, -10f) * Time.deltaTime);
+        transform.Rotate(new Vector3(X, Y, Z) * Time.deltaTime);
     }
 }
