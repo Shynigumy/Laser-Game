ebf3c5e262783150c344c84178631cd46a5cf191
JeanPierreSantMarc <79708078+JeanPierreSantMarc@users.noreply.github.com>
2021-03-21 22:37:21 +0100

    ShopEscalable+VibracionCam

---
+++ Laser Game/Assets/Scripts/Timershake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timershake : MonoBehaviour
+{
+
+    public float TiempoExplocion = 5f;
+    public float tiempo = 0f;
+
+    public GameObject temblor;
+    public GameObject SonidoExplosion;
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo += Time.deltaTime;
+
+        if (tiempo >= TiempoExplocion)
+        {
+            temblor.GetComponent<VibrarCamara>().shouldShake = true;
+            SonidoExplosion.GetComponent<AudioSource>().Play(1);
+            tiempo = 0;
+        }
+
+        
+    }
+}
